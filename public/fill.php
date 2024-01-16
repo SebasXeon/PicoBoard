af12a0fcab70e4ed5def92ed4f7fbe15aa2b17ce
Sebastian Barrero <juansebastian727@gmail.com>
2024-01-15 22:57:59 -0500

    Add componenter.php and update fill.php

--- public/fill.php
+++ public/fill.php
@@ -10,27 +10,23 @@ require_once('app/core/database.php');
 // Include models
 require_once('app/models/BoardModel.php');
 require_once('app/models/AttachmentModel.php');
-require_once('app/models/ThreadModel.php');
 require_once('app/models/PostModel.php');
 
 // Initialize the database connection
 $DB = new Database();
 
-// Create a new thread
-$thread = new Thread($DB);
-$thread->create(1, 'Test thread');
-// Create a new post
-$post = new Post($DB);
-$post->create($thread->id, null, 'Test post');
-// Create another post
-$post = new Post($DB);
-$post->create($thread->id, null, 'Another test post');
-// Edit the post body
-$post->body = 'Edited post body';
-$post->update();
+// Create a post (thread)
+//$post = new Post($DB);
+//$post->create(1, true, null, null, 'Test thread', 'This is a test thread');
+
+// For loop to create 10 replies
+for ($i = 0; $i < 3; $i++) {
+    // Create a post (reply)
+    $post = new Post($DB);
+    $post->create(1, false, 3, null, 'Test reply', 'This is a test reply');
+}
 
 echo 'Done!';
 echo '<pre>';
-var_dump($thread);
 var_dump($post);
 echo '</pre>';
